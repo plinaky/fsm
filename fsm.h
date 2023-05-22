e6e2037b874b759fcac39f34e96ea6b0773e8960
vpinel <vincent.pinel@valeo.com>
2023-05-22 09:46:47 +0200

    very basic fsm
    Signed-off-by: vpinel <vincent.pinel@valeo.com>

---
+++ fsm.h
@@ -0,0 +1,31 @@
+#ifndef _FSM_
+#define _FSM_
+
+struct fsm {
+	void (*state)(struct fsm *myfsm);
+	void *data;
+};
+
+
+#define FSM_DECLARE(_FSM_) struct fsm _FSM_
+
+#define FSM_STATE(_STATE_, _FSM_) static void _STATE_(struct fsm *_FSM_)
+
+#define FSM_NEXT(_FSM_, _STATE_) do {  \
+	(_FSM_)->state = _STATE_;      \
+	return;                        \
+} while(0)
+
+#define FSM_STOP_STATE NULL
+
+#define FSM_STOP(_FSM_) FSM_NEXT(_FSM_, FSM_STOP_STATE)
+
+static inline void FSM_RUN(struct fsm *myfsm, void (*start)(struct fsm *))
+{
+	myfsm->state = start;
+
+	while (myfsm->state != NULL)
+		myfsm->state(myfsm);
+}
+
+#endif
